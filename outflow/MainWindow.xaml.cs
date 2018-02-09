660d7db074eadd553409a0f85e7a87d589d8bb06
Никита Романюк <nikkonrom@gmail.com>
2018-02-09 03:19:29 +0300

    Fixed a progress bar binding behavior

--- outflow/MainWindow.xaml.cs
+++ outflow/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
 using Microsoft.Win32;
+using MonoTorrent.Client;
 using MonoTorrent.Common;
 
 namespace Outflow
@@ -26,7 +28,8 @@ namespace Outflow
     public partial class MainWindow : MetroWindow
     {
         public ObservableCollection<TorrentWrapper> TorrentsList { get; set; }
-
+        private ClientEngine engine = new ClientEngine(new EngineSettings());
+       
         public MainWindow()
         {
             this.TorrentsList = new ObservableCollection<TorrentWrapper>();
@@ -49,16 +52,33 @@ namespace Outflow
                 dialogWIndow.WindowStartupLocation = WindowStartupLocation.CenterOwner;
                 dialogWIndow.ShowDialog();
                 if (dialogWIndow.DialogResult == true)
+                {
                     TorrentsList.Add(new TorrentWrapper(dialogWIndow.DownloadFolderPath.Text, torrent));
-                
-
-
+                    engine.Register(TorrentsList.Last().Manager);
+                }
             }
         }
 
         private void TorrentsDataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
         {
             e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+        
+
+        private async void StartDownloadButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (TorrentsDataGrid.SelectedItem != null)
+            {
+                TorrentWrapper selectedWrapper = (TorrentWrapper)TorrentsDataGrid.SelectedItem;
+                var progress = new Progress<double>(value => selectedWrapper.Progress = value);
+                double result = await Task.Factory.StartNew(() => selectedWrapper.Download(progress),
+                    creationOptions: TaskCreationOptions.LongRunning);
+                selectedWrapper.Progress = result;
+                selectedWrapper.Manager.TorrentStateChanged += delegate(object o, TorrentStateChangedEventArgs args)
+                {
+                    engine.DiskManager.Flush(selectedWrapper.Manager);
+                };
+            }
         }
     }
 }
--- outflow/TorrentWrapper.cs
+++ outflow/TorrentWrapper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,16 +11,51 @@ using MonoTorrent.Common;
 
 namespace Outflow
 {
-    public class TorrentWrapper
+    public class TorrentWrapper : INotifyPropertyChanged
     {
         public Torrent Torrent { get; }
         public TorrentManager Manager { get; }
-        
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double Download(IProgress<double> progress)
+        {
+            Manager.Start();
+            Manager.PieceHashed += delegate (object sender, PieceHashedEventArgs args)
+                {
+                    progress.Report(Manager.Progress);
+                };
+            return Manager.Progress;
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool SetField<T>(ref T field, T value, string propertyName)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        private double progress;
+        //private 
+
+        public double Progress
+        {
+            get => progress;
+            set => SetField(ref progress, value, "Progress");
+        }
+
+
         public TorrentWrapper(string downloadFolderPath, Torrent torrent)
         {
             this.Torrent = torrent;
             this.Manager = new TorrentManager(this.Torrent, downloadFolderPath, new TorrentSettings());
-            
         }
 
     }
